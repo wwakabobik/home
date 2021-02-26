42365c78b3cec6e074a7de5982e751c087d8f4cb
wwakabobik <i.vereshchagin@gmail.com>
2021-02-26 14:39:38 +0300

    - Added WU error condition check (can't retrieve reference data) - Added send data check (obsolete, not updated for timeout) - Added force reboot of ESP8266 modules

--- home_server/secure_data.py
+++ home_server/secure_data.py
@@ -9,6 +9,8 @@ wu_api_key =
 wu_station_id =
 wu_station_pwd =
 wu_reference_station_id =
+wu_cam_id =
+wu_cam_pwd =
 
 # OPEN WEATHER DATA
 ow_api_key =
@@ -17,3 +19,9 @@ ow_station_id =
 # PWSWEATHER DATA
 pwsw_station_id =
 pwsw_api_key =
+
+# NARODMON DATA
+narodmon_name =
+narodmon_owner =
+narodmon_mac =
+narodmon_api_key =
